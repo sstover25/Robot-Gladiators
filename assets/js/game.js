de48a7b5b4175740ac59233b6fb87928629c6253
SSto <95592688+sstover25@users.noreply.github.com>
2022-01-10 13:30:20 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,13 +73,27 @@ var fight = function(enemyName) {
         }
         // else {
         //     window.alert("You need to choose a valid option. Try again!");
-        }
+        // }
     }
-};
+}
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    // call fight function with enemy-robot
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        // let player know what round they are in
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        //reset enemyHealth before starting new fight
+        enemyHealth = 50;
+        
+        debugger;
+
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
